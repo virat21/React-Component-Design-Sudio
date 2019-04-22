2b4e64ff8de948cfacc59b79443b33666476dc8d
Virat Patel <21.virat@gmail.com>
2019-04-22 23:10:31 +0530

    Updates

---
+++ precache-manifest.59a29c41d5322431ce8f1cc1ad72268d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3f5d115cd0572cc6b09b",
+    "url": "/static/js/main.4f446455.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "3f5d115cd0572cc6b09b",
+    "url": "/static/css/main.42f69459.chunk.css"
+  },
+  {
+    "revision": "57fd339df7f44299e0f203e0b81e31fd",
+    "url": "/index.html"
+  }
+];
